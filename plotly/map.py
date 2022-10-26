f4dffbb0bfc2f10a92dffb7e3384843d722c43ef
sw882882 <seungwan.wee@gmail.com>
2022-10-26 12:49:32 +0700

    multiple years dict

--- plotly/map.py
+++ plotly/map.py
@@ -4,23 +4,36 @@ import pandas as pd
 df = pd.read_excel(
     "cleaned data/oil/processed Annual crude and lease condensate exports.xlsx"
 )
-
-y = 1
-yearLocation = []
-while df["value"].tolist():
-    if y >= len(df.index):
-        break
-    if df.iat[y, 3] == 2003:
-        yearLocation.append(y)
-    y += 1
-
+year = df.at(1, "year")
 dfdata = []
-yy = 0
-while yearLocation:
-    if yy >= len(yearLocation):
-        break
-    dfdata.append(df.iat[yearLocation[yy], 4])
-    yy += 1
+data_slider = {}
+while year <= df["year"].tail(1):
+    y = 1
+    yearLocation = []
+    while df["value"].tolist():
+        if y >= len(df.index):
+            break
+        if df.iat[y, 3] == year:
+            yearLocation.append(y)
+        y += 1
+
+    yy = 0
+    while yearLocation:
+        if yy >= len(yearLocation):
+            break
+        dfdata.append(df.iat[yearLocation[yy], 4])
+        yy += 1
+    yearLocation.clear()
+    appendDict = {
+                str(year): dfdata
+            }
+    data_slider.append(appendDict)
+    appendDict.clear()
+    dfdata.clear()
+    year += 1
+
+
+# TODO: adapt the year dict into slider
 fig = go.Figure(
     data=go.Choropleth(
         locations=df["ISO_3_codes"],  # iso code country names
