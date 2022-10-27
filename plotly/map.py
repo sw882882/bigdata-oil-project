5643c78598dd383c302e479dbbd2c77b9f09ce63
sw882882 <seungwan.wee@gmail.com>
2022-10-27 17:56:41 +0700

    slider changes

--- plotly/map.py
+++ plotly/map.py
@@ -1,13 +1,14 @@
 import plotly.graph_objects as go
 import pandas as pd
-
+import plotly.offline as offline
+print("starting...")
 df = pd.read_excel(
     "cleaned data/oil/processed Annual crude and lease condensate exports.xlsx"
 )
-year = df.at(1, "year")
-dfdata = []
-data_slider = {}
-while year <= df["year"].tail(1):
+year = df.at[1, "year"]
+df_one_year = []
+data_slider = []
+while year <= df["year"].tolist()[-1]:
     y = 1
     yearLocation = []
     while df["value"].tolist():
@@ -21,19 +22,65 @@ while year <= df["year"].tail(1):
     while yearLocation:
         if yy >= len(yearLocation):
             break
-        dfdata.append(df.iat[yearLocation[yy], 4])
+        df_one_year.append(df.iat[yearLocation[yy], 4])
         yy += 1
     yearLocation.clear()
-    appendDict = {
-                str(year): dfdata
-            }
-    data_slider.append(appendDict)
-    appendDict.clear()
-    dfdata.clear()
+
+    data_one_year_dict = dict(
+        type="choropleth",
+        locations=df["ISO_3_codes"],
+        z=df_one_year,
+        locationmode="ISO-3",
+        colorscale="Reds",
+    )
+
+    data_slider.append(data_one_year_dict)
+
+    df_one_year.clear()
     year += 1
 
 
+steps = []
+for i in range(len(data_slider)):
+    step = dict(
+        method="restyle",
+        args=["visible", [False] * len(data_slider)],
+        label="Year {}".format(i + df.at[1, "year"]),
+    )
+    step["args"][1][i] = True
+    steps.append(step)
+
+# I create the 'sliders' object from the 'steps'
+
+sliders = [dict(active=0, pad={"t": 1}, steps=steps)]
+
 # TODO: adapt the year dict into slider
+
+layout = dict(
+
+    geo=dict(
+        scope="world",
+    ),
+    sliders=sliders,
+)
+
+geo = dict(
+        showcountries=True,
+        countrycolor="RebeccaPurple"
+        )
+
+fig = dict(data=data_slider, layout=layout, geo=geo)
+
+offline.plot(
+    fig,
+    auto_open=True,
+    image_filename="oil_exports",
+    filename="./output.html",
+    validate=True,
+)
+
+
+"""
 fig = go.Figure(
     data=go.Choropleth(
         locations=df["ISO_3_codes"],  # iso code country names
@@ -50,6 +97,8 @@ fig.update_geos(showcountries=True, countrycolor="RebeccaPurple")
 fig.update_layout(
     title_text="Global crude oil exports 2003",
     geo_scope="world",  # limite map scope to USA
+    sliders=sliders,
 )
 
 fig.show()
+"""
