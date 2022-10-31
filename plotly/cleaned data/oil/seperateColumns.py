af1adfd5e85bfdfece431ce1ef4b48f93fe9aeb8
sw882882 <seungwan.wee@gmail.com>
2022-10-31 14:07:22 +0700

    finish script cleaning data

---
+++ plotly/cleaned data/oil/seperateColumns.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+inputFile = input("desired file: ")
+df = pd.read_excel(inputFile)
+
+list = []
+
+
+
+outputFile = input("desired output file: ")
+print("complete, exiting...")
